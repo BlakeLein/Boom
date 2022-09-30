94b69de47d0c6d6d38d4be9c1ae3843a18912dec
Blake Lein <blake.lein@gmail.com>
2022-09-30 12:37:52 -0500

    Tweaks

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { config, useClient, useMicrophoneAndCameraTracks } from "../server/agora";
+import {
+  config,
+  useClient,
+  useMicrophoneAndCameraTracks,
+} from "../server/agora";
 import { RootState } from "../store";
 import Video from "./Video";
 
@@ -24,7 +28,8 @@ type Styles = {
 
 const styles = {} as Styles;
 
-styles.static = "w-full lg:col-start-1 lg:col-span-full lg:row-start-2 lg:row-span-1 h-full p-2 md:p-3 lg:p-4 bg-neutral-300 rounded";
+styles.static =
+  "w-full lg:col-start-1 lg:col-span-full lg:row-start-2 lg:row-span-1 h-full p-2 md:p-3 lg:p-4 bg-whote rounded";
 
 export default function Gallery({ galleryRef, className = "" }: Props) {
   const user = {
@@ -71,7 +76,8 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
             if (mediaType === "video")
               try {
                 videoStateUsersRef.current.forEach((stream: any) => {
-                  if (stream.uid === user.uid) throw new Error("duplicate user");
+                  if (stream.uid === user.uid)
+                    throw new Error("duplicate user");
                 });
                 dispatch(video.actions.addUser(user));
                 const publishSound = new Audio("/sounds/publish.mp3");
@@ -85,13 +91,15 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
 
           client.on("stream-unpublished", (user: any, mediaType: any) => {
             user.audioTrack && user.audioTrack.stop();
-            if (mediaType === "video") user.videoTrack && user.videoTrack.stop();
+            if (mediaType === "video")
+              user.videoTrack && user.videoTrack.stop();
             console.log("HERE", "STREAM-PUBLISHED");
           });
 
           client.on("stream-removed", (user: any, mediaType: any) => {
             user.audioTrack && user.audioTrack.stop();
-            if (mediaType === "video") user.videoTrack && user.videoTrack.stop();
+            if (mediaType === "video")
+              user.videoTrack && user.videoTrack.stop();
             console.log("HERE", "STREAM-REMOVED");
           });
 
@@ -108,7 +116,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
           });
         }
         if (curState === "DISCONNECTED") {
-          client.on("user-left", user => {
+          client.on("user-left", (user) => {
             dispatch(video.actions.removeUser(user));
             console.log("HERE", `${user.uid} LEFT`);
           });
@@ -128,7 +136,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
     return () => {
       dispatch(video.actions.setUsers([]));
 
-      tracks && tracks.forEach(track => track.close());
+      tracks && tracks.forEach((track) => track.close());
       client.leave().then(() => {
         client.removeAllListeners();
       });
@@ -137,7 +145,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
   }, [location]);
 
   const handlePoints = (uid: any) => {
-    const player = game.state.players.find(player => player.player === uid);
+    const player = game.state.players.find((player) => player.player === uid);
     if (!player) return;
     return player.points;
   };
@@ -148,14 +156,29 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
     <Component id="Gallery">
       <div ref={galleryRef} className={`${styles.static} ${styles.dynamic}`}>
         <Container>
-          <div className={`flex flex-col xl:flex-row 6xl:flex-col justify-center items-center h-full gap-2 md:gap-3 lg:gap-4`}>
+          <div
+            className={`flex flex-col xl:flex-row 6xl:flex-col justify-center items-center h-full gap-2 md:gap-3 lg:gap-4`}
+          >
             {tracks && (
               <div className="contents">
-                <Video tracks={tracks} active={true} username={user.state.userName} points={game.state.playerPoints} />
+                <Video
+                  tracks={tracks}
+                  active={true}
+                  username={user.state.userName}
+                  points={game.state.playerPoints}
+                />
                 {video.state.users &&
-                  video.state.users.map(user => {
+                  video.state.users.map((user) => {
                     const points = handlePoints(user.uid);
-                    return <Video tracks={[user.audioTrack, user.videoTrack]} username={user.uid} key={user.uid} points={points} active={false} />;
+                    return (
+                      <Video
+                        tracks={[user.audioTrack, user.videoTrack]}
+                        username={user.uid}
+                        key={user.uid}
+                        points={points}
+                        active={false}
+                      />
+                    );
                   })}
               </div>
             )}
--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -12,7 +12,13 @@ import Component from "../components/Component";
 
 import { AgoraVideoPlayer } from "agora-rtc-react";
 
-import { faMicrophone, faMicrophoneSlash, faStar, faVideoCamera, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMicrophone,
+  faMicrophoneSlash,
+  faStar,
+  faVideoCamera,
+  faVideoSlash,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
@@ -39,7 +45,13 @@ styles.conditional = {
   inactive: "bg-neutral-200",
 };
 
-export default function Video({ active, className = null, tracks, username, points }: Props) {
+export default function Video({
+  active,
+  className = null,
+  tracks,
+  username,
+  points,
+}: Props) {
   const video = {
     state: useSelector((state: RootState) => state.video),
     actions: videoSlice.actions,
@@ -55,33 +67,54 @@ export default function Video({ active, className = null, tracks, username, poin
     <Component id="Video">
       <div className={`${styles.static} ${styles.dynamic}`}>
         <div className="h-full w-full object-cover object-center">
-          <AgoraVideoPlayer videoTrack={tracks[1]} className="w-full h-full scale-105" />
+          <AgoraVideoPlayer
+            videoTrack={tracks[1]}
+            className="w-full h-full scale-105"
+          />
         </div>
         <div className="absolute inset-0 flex justify-start items-end">
-          <div className={`w-full md:p-2 lg:p-4 flex justify-between items-center ${active ? styles.conditional.active : styles.conditional.inactive}`}>
+          <div
+            className={`w-full md:p-2 h-4 lg:p-4 flex justify-between items-center ${
+              active ? styles.conditional.active : styles.conditional.inactive
+            }`}
+          >
             <button
-              className={`${!active && "invisible"} w-8 flex justify-center items-center`}
+              className={`${
+                !active && "invisible"
+              } w-8 flex justify-center items-center`}
               onClick={async () => {
                 if (!active) return;
                 await tracks[0].setEnabled(!video.state.microphone);
                 dispatch(video.actions.setMicrophone(!video.state.microphone));
               }}
             >
-              <FontAwesomeIcon icon={video.state.camera && video.state.microphone ? faMicrophone : faMicrophoneSlash} className="text-xs text-inherit" />
+              <FontAwesomeIcon
+                icon={
+                  video.state.camera && video.state.microphone
+                    ? faMicrophone
+                    : faMicrophoneSlash
+                }
+                className="text-xs text-inherit"
+              />
             </button>
             <span className="w-full text-xs text-inherit text-center">
               {`${username} - ${points} `}
               <FontAwesomeIcon icon={faStar} className="text-amber-300" />
             </span>
             <button
-              className={`${!active && "invisible"} w-8 justify-center items-center`}
+              className={`${
+                !active && "invisible"
+              } w-8 justify-center items-center`}
               onClick={async () => {
                 if (!active) return;
                 await tracks[1].setEnabled(!video.state.camera);
                 dispatch(video.actions.setCamera(!video.state.camera));
               }}
             >
-              <FontAwesomeIcon icon={video.state.camera ? faVideoCamera : faVideoSlash} className="text-xs text-inherit" />
+              <FontAwesomeIcon
+                icon={video.state.camera ? faVideoCamera : faVideoSlash}
+                className="text-xs text-inherit"
+              />
             </button>
           </div>
         </div>
